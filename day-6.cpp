8724856c62a020ddc588121b0673fef5ab56f3db
Alice Ekblad <alice.ekblad@outlook.com>
2024-01-08 16:34:28 +0100

    add day 6 part 1

---
+++ day-6.cpp
@@ -0,0 +1,77 @@
+// Day 6
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+vector<int> extractNums(string sNums) {
+    vector<int> nums{};
+    stringstream ss;
+    string temp;
+    int num;
+
+    // Storing the whole string into string stream
+    ss << sNums;
+
+    //Running loop till the end of the stream
+    while (!ss.eof()) {
+        // Extracting number by number from stream
+        ss >> num;  // OBS: 
+        nums.push_back(num);
+    }
+
+    return nums;
+}
+
+int main() {
+    vector<int> times;
+    vector<int> distances;
+
+    // Go through file line by line
+    ifstream file{"inputs/day-6-input.txt"};
+    string line;
+    while (getline(file, line))
+    {
+        // Read in numbers
+        auto idx = line.find(":");
+        string numbers = line.substr(idx + 1, line.size());
+
+        if (line.find("Time") != string::npos) {
+            // Place in times
+            times = extractNums(numbers);
+        } else {
+            // Place in distances
+            distances = extractNums(numbers);
+        }
+    }
+
+    // Go through each race
+    int marginOfError{1};
+    for (int i{0}; i < times.size(); i++) {
+        int time{times[i]};
+        int distance{distances[i]};
+
+        // Brute-force: Go thorugh each option and calc travelled distance
+        int countWins{0};
+        for (int t{1}; t < time; t++) {
+            int speed{t};
+            int timeToTravel{time - t};
+            int travelDistance{speed * timeToTravel};
+
+            if (travelDistance > distance) {
+                countWins++;
+            }
+        }
+
+        // Multiply to margin of errors
+        marginOfError *= countWins;
+    }
+
+    cout << "Result: " << marginOfError << endl;
+
+    return 0;
+}
